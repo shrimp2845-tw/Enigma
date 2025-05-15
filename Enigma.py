d88333f9f747d6f3588a7c47f06a6ac8ae1c1039
shrimp2845-tw <goldencheng15@gmail.com>
2025-05-15 14:52:54 +0800

    Update Enigma.py

--- Enigma.py
+++ Enigma.py
@@ -123,7 +123,7 @@ def originalengima():
     else:
         setup=readsetup(filename)
         rotor1,rotor2,rotor3,reflector,plugboard=setup
-        offset3,offset2,offset1=[ord(i)-97 for i in input("Please enter 3 offset for rotor(such as abc,mei,qaq):")]
+        offset3,offset2,offset1=[ord(i.lower())-97 for i in input("Please enter 3 offset,case doesn't matter(such as ABC,MEI,QAQ):")]
         rotor1=rotors(rotor1,offset1)
         rotor2=rotors(rotor2,offset2)
         rotor3=rotors(rotor3,offset3)

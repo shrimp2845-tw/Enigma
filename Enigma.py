4233fb7328fce335b9229140b88c4fa3cc65c5f1
shrimp2845-tw <goldencheng15@gmail.com>
2025-07-07 20:33:16 +0800

    Update Enigma.py

--- Enigma.py
+++ Enigma.py
@@ -1,98 +1,110 @@
 import random as rd
 import ast
 import os
-dir=os.path.dirname(os.path.abspath(__file__))
-class rotors():
-    def __init__(self,rotor,offset):
-        self.offset=offset%len(rotor)
+
+dir = os.path.dirname(os.path.abspath(__file__))
+
+
+class Rotors:
+    def __init__(self, rotor, offset):
+        self.offset = offset % len(rotor)
         for i in range(offset):
-            rotor.insert(0,rotor.pop())
-        self.rotor=rotor
-    def left(self,node):
-        while node<=-len(self.rotor):
-            node-=-len(self.rotor)
-        node+=self.rotor[node][1]
+            rotor.insert(0, rotor.pop())
+        self.rotor = rotor
+
+    def left(self, node):
+        while node <= -len(self.rotor):
+            node -= -len(self.rotor)
+        node += self.rotor[node][1]
         return node
-    def right(self,node):
-        while node<=-len(self.rotor):
-            node-=-len(self.rotor)
-        node+=self.rotor[node][0]
+
+    def right(self, node):
+        while node <= -len(self.rotor):
+            node -= -len(self.rotor)
+        node += self.rotor[node][0]
         return node
+
     def shift(self):
-        self.offset=(self.offset + 1) % len(self.rotor)
+        self.offset = (self.offset + 1) % len(self.rotor)
         self.rotor.insert(0, self.rotor.pop())
+
     def get_offset(self):
-        alphalist=[chr(i) for i in range(97,97+len(self.rotor))]
-        return (alphalist[self.offset])
+        alphalist = [chr(i) for i in range(97, 97 + len(self.rotor))]
+        return alphalist[self.offset]
+
     @staticmethod
     def randrotor(n):
-        nodes=list(range(n))
-        spinwheel=[[1, 1] for i in range(n)]
+        nodes = list(range(n))
+        spinwheel = [[1, 1] for i in range(n)]
         for i in range(n):
-            node=rd.choice(nodes)
+            node = rd.choice(nodes)
             nodes.remove(node)
-            move1=node-i
-            if move1>0:
-                move1=move1-n
-            spinwheel[i][1]=move1
-            spinwheel[i+move1][0]=-n-move1
+            move1 = node - i
+            if move1 > 0:
+                move1 = move1 - n
+            spinwheel[i][1] = move1
+            spinwheel[i + move1][0] = -n - move1
         return spinwheel
 
 
-class plugboards():
-    def __init__(self,l):
-        self.plugboard={}
+class Plugboards:
+    def __init__(self, l):
+        self.plugboard = {}
         for i in l:
-            self.plugboard[i[0]]=i[1]
-            self.plugboard[i[1]]=i[0]
+            self.plugboard[i[0]] = i[1]
+            self.plugboard[i[1]] = i[0]
+
     @staticmethod
-    def randplugboard(n,m):
-        lines=[]
-        nodes = [chr(j) for j in range(97, 97+n)]
+    def randplugboard(n, m):
+        lines = []
+        nodes = [chr(j) for j in range(97, 97 + n)]
         for i in range(m):
-            line=rd.sample(nodes,k=2)
+            line = rd.sample(nodes, k=2)
             lines.append(line)
             for j in line:
                 nodes.remove(j)
         return lines
-    def pluged(self,alpha):
-        changedalpha=self.plugboard.get(alpha)
+
+    def pluged(self, alpha):
+        changedalpha = self.plugboard.get(alpha)
         if changedalpha:
             return changedalpha
         return alpha
 
 
-class reflectors():
-    def __init__(self,l):
-        self.reflector=l
+class Reflectors:
+    def __init__(self, l):
+        self.reflector = l
+
     @staticmethod
     def randreflector(n):
-        lines=[]
-        nodes=[j for j in range(0,n)]
-        for i in range(n//2):
-            line=rd.sample(nodes,k=2)
+        lines = []
+        nodes = [j for j in range(0, n)]
+        for i in range(n // 2):
+            line = rd.sample(nodes, k=2)
             lines.append(line)
             for j in line:
                 nodes.remove(j)
         return lines
-    def reflect(self,node):
-        while node<0:
-            node=len(self.reflector*2)-abs(node)
+
+    def reflect(self, node):
+        while node < 0:
+            node = len(self.reflector * 2) - abs(node)
         for i in self.reflector:
             if node in i:
-                return i[i.index(node)-1]
+                return i[i.index(node) - 1]
 
 
 def randsetup(filename):
     global dir
     os.makedirs(fr"{dir}/data", exist_ok=True)
-    setup=[]
+    setup = []
     for i in range(3):
-        setup.append(rotors.randrotor(26))
-    setup.append(reflectors.randreflector(26))
-    setup.append(plugboards.randplugboard(26,6))
-    path=fr'{dir}/data/{filename}.txt'
-    setupfile=open(path,"w")
+        setup.append(Rotors.randrotor(26))
+    setup.append(Reflectors.randreflector(26))
+    setup.append(Plugboards.randplugboard(26, 6))
+    path = fr'{dir}/data/{filename}.txt'
+    setupfile = open(path, "w")
     setupfile.write(str(setup))
     setupfile.close()
     print("success")
@@ -101,9 +113,9 @@ def randsetup(filename):
 def readsetup(filename):
     global dir
     os.makedirs(fr"{dir}/data", exist_ok=True)
-    path=fr'{dir}/data/{filename}.txt'
-    setupfile=open(path,'r')
-    data=ast.literal_eval(setupfile.read())
+    path = fr'{dir}/data/{filename}.txt'
+    setupfile = open(path, 'r')
+    data = ast.literal_eval(setupfile.read())
     setupfile.close()
     return data
 
@@ -111,49 +123,52 @@ def readsetup(filename):
 def originalengima():
     global dir
     os.makedirs(fr"{dir}/data", exist_ok=True)
-    filename=input('Please enter your setup file name:')
-    path=fr'{dir}/data/{filename}.txt'
+    filename = input('Please enter your setup file name:')
+    path = fr'{dir}/data/{filename}.txt'
     print(f'Please check the path:{path}')
     if not os.path.exists(path):
         print("File does not exist")
     else:
-        setup=readsetup(filename)
-        rotor1,rotor2,rotor3,reflector,plugboard=setup
-        offset3,offset2,offset1=[ord(i.lower())-97 for i in input("Please enter 3 offset,case doesn't matter(such as ABC,MEI,QAQ):")]
-        rotor1=rotors(rotor1,offset1)
-        rotor2=rotors(rotor2,offset2)
-        rotor3=rotors(rotor3,offset3)
-        reflector=reflectors(reflector)
-        plugboard=plugboards(plugboard)
+        setup = readsetup(filename)
+        rotor1, rotor2, rotor3, reflector, plugboard = setup
+        offset3, offset2, offset1 = [ord(i.lower()) - 97 for i in
+                                     input("Please enter 3 offset,case doesn't matter(such as ABC,MEI,QAQ):")]
+        rotor1 = Rotors(rotor1, offset1)
+        rotor2 = Rotors(rotor2, offset2)
+        rotor3 = Rotors(rotor3, offset3)
+        reflector = Reflectors(reflector)
+        plugboard = Plugboards(plugboard)
         print('Setup done!')
-        print('Please enter your text below,only lower-case english character will be encrypted,Multiline input allowed,empty row finished input:')
-        lines=[]
+        print(
+            'Please enter your text below,only lower-case english character will be encrypted,Multiline input allowed,empty row finished input:')
+        lines = []
         while 1:
-            line=input()
-            if line=='':
+            line = input()
+            if line == '':
                 break
             lines.append(line)
-        text='\n'.join(lines)
-        encrypttext=''
-        count=0
-        len_rotor=len(rotor1.rotor)
+        text = '\n'.join(lines)
+        encrypttext = ''
+        count = 0
+        len_rotor = len(rotor1.rotor)
         for i in text:
-            alphalist = [chr(j) for j in range(97, 97+len_rotor)]
+            alphalist = [chr(j) for j in range(97, 97 + len_rotor)]
             if i not in alphalist:
-                encrypttext+=i
+                encrypttext += i
             else:
-                step1=plugboard.pluged(i)
-                step2=rotor1.right(rotor2.right(rotor3.right(reflector.reflect(rotor3.left(rotor2.left(rotor1.left(ord(step1)-97)))))))
-                while step2<0:
-                    step2+=len_rotor
-                step3=chr(97+step2)
-                step4=plugboard.pluged(step3)
-                encrypttext+=step4
-                count+=1
+                step1 = plugboard.pluged(i)
+                step2 = rotor1.right(rotor2.right(
+                    rotor3.right(reflector.reflect(rotor3.left(rotor2.left(rotor1.left(ord(step1) - 97)))))))
+                while step2 < 0:
+                    step2 += len_rotor
+                step3 = chr(97 + step2)
+                step4 = plugboard.pluged(step3)
+                encrypttext += step4
+                count += 1
                 rotor1.shift()
-                if count%len_rotor==0:
+                if count % len_rotor == 0:
                     rotor2.shift()
-                if count%(len_rotor*len_rotor)==0:
+                if count % (len_rotor * len_rotor) == 0:
                     rotor3.shift()
         print('Here is encrypted text:')
         print(encrypttext)
@@ -163,7 +178,7 @@ def main():
     try:
         originalengima()
     except Exception as e:
-        print('ERROR:',e)
+        print('ERROR:', e)
     finally:
         input('----------Press enter to exit----------')
 
